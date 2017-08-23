6f3a5164d3f4baa0418435a223ba9eb0c23f12d3
Afanasy Argunov <afanasy@argunov.com>
2017-08-23 11:13:48 +0900

    0.0.4

--- refbook/index.js
+++ refbook/index.js
@@ -41,57 +41,63 @@ exports.sync = async config => {
             return Object.assign({}, j.value);
         }));
         rl = rl.map(i => i.map(j => Object.assign({}, { value: j.$value })));
-        await rl.reduce((p, i) => p.then(async () => {
-            try {
-                let d = await Refbook.findOne({ code: i[0].value });
-                if (!d && (i[0].value === 'C33001' || i[0].value === 'C33002')) {
-                    let rf = new Refbook({
-                        code: i[0].value,
-                        name: i[1].value,
-                        oid: i[2].value,
-                        version: i[3].value
-                    });
-                    await rf.save();
-                }
-            } catch (e) { console.error(e); }
-            return i;
-        }), Promise.resolve());
+        for (let i of rl) {
+            let d = await Refbook.findOne({ code: i[0].value });
+            if (!d && (i[0].value === 'C33001' || i[0].value === 'C33002' || i[0].value === 'MDP365')) {
+                let rf = new Refbook({
+                    code: i[0].value,
+                    name: i[1].value,
+                    oid: i[2].value,
+                    version: i[3].value
+                });
+                await rf.save();
+            }
+        }
         rl = await Refbook.find();
         let result = [];
-        await rl.reduce((p, i) => p.then(async () => {
-            try {
-                let parts = await rias.getRefbookParts({
-                    code: i.code,
-                    version: i.version
-                });
-                let record = await rias.getRefbookPartial({
-                    code: i.code,
-                    version: i.version,
-                    part: parts.getRefBookPartsReturn
-                });
-                record = record.getRefBookPartialReturn.map(i => i.map.item.map(j => {
-                    return Object.assign({}, j.value);
-                }));
-                record = record.map(i => i.map(j => Object.assign({}, { value: j.$value })));
-                record.reduce((p, j) => p.then(async () => {
-                    try {
-                        let d = await Record.findOne({ code: j[0].value });
-                        if (!d) {
-                            let rc = new Record({
-                                _refbook: i._id,
-                                code: j[0].value,
-                                high: j[1].value,
-                                name: j[3].value
-                            });
-                            await rc.save();
-                            result.push(rc);
-                        }
-                    } catch (e) { console.error(e); }
-                    return j;
-                }), Promise.resolve());
-            } catch (e) { console.error(e); }
-            return i;
-        }), Promise.resolve());
+        for (let i of rl) {
+            let parts = await rias.getRefbookParts({
+                code: i.code,
+                version: i.version
+            });
+            let record = await rias.getRefbookPartial({
+                code: i.code,
+                version: i.version,
+                part: parts.getRefBookPartsReturn
+            });
+            record = record.getRefBookPartialReturn.map(i => i.map.item.map(j => {
+                return Object.assign({}, j.value);
+            }));
+            record = record.map(i => i.map(j => Object.assign({}, { value: j.$value })));
+            if (i.code === 'MDP365') {
+                for (let j of record) {
+                    let d = await Record.findOne({ code: j[2].value });
+                    if (!d) {
+                        let rc = new Record({
+                            _refbook: i._id,
+                            code: j[2].value,
+                            name: j[3].value
+                        });
+                        await rc.save();
+                        result.push(rc);
+                    }
+                }
+            } else {
+                for (let j of record) {
+                    let d = await Record.findOne({ code: j[0].value });
+                    if (!d) {
+                        let rc = new Record({
+                            _refbook: i._id,
+                            code: j[0].value,
+                            high: j[1].value,
+                            name: j[3].value
+                        });
+                        await rc.save();
+                        result.push(rc);
+                    }
+                }
+            }
+        }
         return { data: result, mongoose: mongoose };
     } catch (e) { return e; }
 };
